e8022d489e8f483cf353be93cefb131cbb1f6de1
Chiemela Osumgba <wisdom@Andelas-MacBook-Pro.local>
2021-05-17 20:50:07 +0100

    starter files

---
+++ lib/intro/intro.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Intro extends StatefulWidget {
+  const Intro({Key key}) : super(key: key);
+
+  @override
+  _IntroState createState() => _IntroState();
+}
+
+class _IntroState extends State<Intro> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/intro/splash.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key key}) : super(key: key);
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
